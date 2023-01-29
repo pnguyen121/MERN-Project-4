bc1cb3c844eaebfc39fa43bdb1ead59ee8d7e509
pnguyen121 <phillynguyen17@gmail.com>
2023-01-29 16:52:19 -0500

    Created login and signup forms that are semi functional, saving progress

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,12 +1,88 @@
-import React from 'react';
-import './LoginPage.css';
+import React, { useState } from "react";
+import "./LoginPage.css";
+
+import { Link } from "react-router-dom";
 
 
-export default function LoginPage(props){
-   
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Image,
+  Message,
+  Segment,
+  Checkbox,
+} from "semantic-ui-react";
 
-    return (
-      <div>Login Pageeeeee</div>
-      );
+export default function LoginPage(props) {
+
+  // State for the form inputs and such
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+  })
+
+  const [error, setError] = useState('')
+
+
+
+
+  // Function to handle what is being typed into form to be in state. Use this function in form
+  function handleChange(e){
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+
+
+
+  return (
+    <Grid
+      textAlign="center"
+      style={{ height: "100vh", width: "100vw" }}
+      verticalAlign="middle"
+    >
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as="h2" color="black" textAlign="center">
+          <Image src="https://i.imgur.com/ZIrCjzu.png" /> Log-in to your account
+        </Header>
+        <Form>
+          <Segment stacked>
+            <Form.Input
+              type="email"
+              name="email"
+              placeholder="email"
+              value={state.email}
+              onChange={handleChange}
+              required
+            />
+            <Form.Input
+              name="password"
+              type="password"
+              placeholder="password"
+              value={state.password}
+              onChange={handleChange}
+              required
+            />
+            <Button
+              color="black"
+              fluid
+              size="large"
+              type="submit"
+              className="btn"
+            >
+              Login
+            </Button>
+          </Segment>
+        </Form>
+        <Message>
+          New Here? <Link to="/signup">Sign Up</Link>
+        </Message>
+        {/* {error ? <ErrorMessage error={error} /> : null} */}
+      </Grid.Column>
+    </Grid>
+  );
 }
-
--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,1 +1,79 @@
+import React from "react";
 
+
+import { Form, Grid, Checkbox, Button, Header, Image, Segment, Message } from "semantic-ui-react";
+
+function SignUpPage() {
+  return (
+    <Grid
+      textAlign="center"
+      style={{ height: "100vh", width: "100vw" }}
+      verticalAlign="middle"
+    >
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as="h2" color="black" textAlign="center">
+          <Image src="https://i.imgur.com/ZIrCjzu.png" /> Sign Up
+        </Header>
+        <Form>
+          <Segment stacked>
+            <Form.Input
+              type="username"
+              name="username"
+              placeholder="username"
+              // value={state.email}
+              // onChange={handleChange}
+              required
+            />
+            <Form.Input
+              type="email"
+              name="email"
+              placeholder="email"
+              // value={state.email}
+              // onChange={handleChange}
+              required
+            />
+            <Form.Input
+              name="password"
+              type="password"
+              placeholder="password"
+              // value={state.password}
+              // onChange={handleChange}
+              required
+            />
+            <Form.Input
+              name="passwordConf"
+              type="password"
+              placeholder="Confirm Password"
+            //   value={state.passwordConf}
+            //   onChange={handleChange}
+              required
+            />
+            <Form.Field>
+              <Form.Input
+                type="file"
+                name="photo"
+                placeholder="upload image"
+                // onChange={handleFileInput}
+              />
+            </Form.Field>
+            <Form.Field required>
+              <Checkbox label="I agree to the Terms and Conditions" />
+            </Form.Field>
+            <Button
+              color="black"
+              fluid
+              size="large"
+              type="submit"
+              className="btn"
+            >
+              Login
+            </Button>
+          </Segment>
+        </Form>
+        {/* {error ? <ErrorMessage error={error} /> : null} */}
+      </Grid.Column>
+    </Grid>
+  );
+}
+
+export default SignUpPage;
